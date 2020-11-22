baf87bb6ccc6957731ca4581caa2938079a8ad8b
Masahiro Tatsuta <52252007+MasaTatsuta@users.noreply.github.com>
2020-11-22 10:26:20 +0900

    commit

--- deploy-me.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd /var/www/tripalbum/ && git reset --hard origin/master && docker-compose run web bundle exec bundle install --path vendor/bundle --without test development && docker-compose run web bundle exec rails assets:precompile && docker-compose up -d
